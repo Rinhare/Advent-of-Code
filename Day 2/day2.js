aa0bfad490d0df11d9d2c6b4b5e85578fd90f65d
Jussi Rinne <jusurinhare@gmail.com>
2023-12-04 17:48:51 +0200

    part 2

--- Day 2/day2.js
+++ Day 2/day2.js
@@ -1,7 +1,32 @@
 const fs = require("fs");
 
-function partOne (file) {
-    const lines = fs.readFileSync(file, "utf-8").trim().split("\n");
-    console.log(lines);
+function partTwo (file) {
+    const lines = fs.readFileSync(file, "utf-8").trim().split(/\r?\n/);
+    // console.log(lines);
+    //    Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    
+    
+    return lines.map((line) =>{
+        const maxCount = {
+            red: 0,
+            green: 0, 
+            blue: 0,
+        };
+        line
+       .split(": ")[1]
+       .split("; ")
+       .forEach((set) => {
+        const pulls = set.split(", ");
+        return pulls.forEach((pull) => {
+            const [count, color] = pull.split(' ');
+            if(maxCount[color] < Number(count)) {
+                maxCount[color] = Number(count)
+            }
+        });
+    });
+    return maxCount.red * maxCount.green * maxCount.blue;
+}).reduce((s, v) => s + v );
 }
-console.log(partOne("./input.txt"));
+
+
+console.log(partTwo("./input.txt"));
